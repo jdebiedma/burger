6dcffe5f76c7486553b149a191c2425d20dc3a3c
Juan DeBiedma <juanthefatty@gmail.com>
2017-05-05 01:09:14 -0400

    test

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN NOT NULL,
-	date TIMESTAMP,
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,,
 	PRIMARY KEY (id)
 );
